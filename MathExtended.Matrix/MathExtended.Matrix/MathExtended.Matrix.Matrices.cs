f7de1aef9a4bbc90130844bf5fba7cba5c01865f
ahotko <ales.hotko@zejn.si>
2018-04-23 10:26:36 +0200

    Refactoring

---
+++ MathExtended.Matrix/MathExtended.Matrix/MathExtended.Matrix.Matrices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Annex.MathExtended.Matrices
+{
+    public partial class Matrix
+    {
+        private void Zero()
+        {
+            for (int _row = 0; _row < _matrix.GetLength(0); _row++)
+                for (int _col = 0; _col < _matrix.GetLength(1); _col++)
+                {
+                    _matrix[_row, _col] = 0.0;
+                }
+        }
+
+        private void Identity()
+        {
+            if (Rows != Columns)
+                throw new InvalidOperationException("Identity matrix must be square.");
+            Zero();
+            for (int n = 0; n < Rows; n++)
+                _matrix[n, n] = 1.0;
+        }
+
+        public static Matrix Zero(int size)
+        {
+            var _result = new Matrix(size);
+            _result.Zero();
+            return _result;
+        }
+
+        public static Matrix Identity(int size)
+        {
+            var _result = new Matrix(size);
+            _result.Identity();
+            return _result;
+        }
+    }
+}
---
+++ MathExtended.Matrix/MathExtended.Matrix/MathExtended.Matrix.Operators.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Annex.MathExtended.Matrices
+{
+    public partial class Matrix
+    {
+        #region Overloaded operators
+        public static Matrix operator ~(Matrix matrix)
+        {
+            var result = matrix.Duplicate();
+            result.Inverse();
+            return result;
+        }
+
+        public static Matrix operator +(Matrix first, Matrix second)
+        {
+            var result = first.Duplicate();
+            result.Add(second);
+            return result;
+        }
+
+        public static Matrix operator *(Matrix first, Matrix second)
+        {
+            var result = first.Duplicate();
+            result.Multiply(second);
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix, double scalar)
+        {
+            var result = matrix.Duplicate();
+            result.Multiply(scalar);
+            return result;
+        }
+
+        public static Matrix operator *(double scalar, Matrix matrix)
+        {
+            var result = matrix.Duplicate();
+            result.Multiply(scalar);
+            return result;
+        }
+
+        public static Boolean operator ==(Matrix first, Matrix second)
+        {
+            bool result = false;
+            result = (first.Rows == second.Rows) && (first.Columns == second.Columns);
+            if (result)
+            {
+                for (int row = 1; row <= first.Rows; row++)
+                {
+                    for (int col = 1; col <= first.Columns; col++)
+                    {
+                        result &= (first[row, col] == second[row, col]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Boolean operator !=(Matrix first, Matrix second)
+        {
+            bool result = false;
+            result = (first.Rows != second.Rows) || (first.Columns != second.Columns);
+            if (!result)
+            {
+                for (int row = 1; row <= first.Rows; row++)
+                {
+                    for (int col = 1; col <= first.Columns; col++)
+                    {
+                        result |= (first[row, col] != second[row, col]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Boolean operator >(Matrix first, Matrix second)
+        {
+            return (first.Rows * first.Columns) > (second.Rows * second.Columns);
+        }
+
+        public static Boolean operator <(Matrix first, Matrix second)
+        {
+            return (first.Rows * first.Columns) < (second.Rows * second.Columns);
+        }
+
+        public static Boolean operator >=(Matrix first, Matrix second)
+        {
+            return (first.Rows * first.Columns) >= (second.Rows * second.Columns);
+        }
+
+        public static Boolean operator <=(Matrix first, Matrix second)
+        {
+            return (first.Rows * first.Columns) <= (second.Rows * second.Columns);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var matrix = obj as Matrix;
+            if (obj == null)
+            {
+                return false;
+            }
+            bool result = false;
+            result = (this.Rows == matrix.Rows) && (this.Columns == matrix.Columns);
+            if (result)
+            {
+                for (int row = 1; row <= this.Rows; row++)
+                {
+                    for (int col = 1; col <= this.Columns; col++)
+                    {
+                        result &= (this[row, col] == matrix[row, col]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            var _matrixString = new StringBuilder();
+            _matrixString.Append(this.Rows).Append("x").Append(this.Columns).Append("=");
+            for (int row = 1; row <= this.Rows; row++)
+            {
+                for (int col = 1; col <= this.Columns; col++)
+                {
+                    _matrixString.Append(this[row, col]).Append(";");
+                }
+            }
+            return _matrixString.ToString().GetHashCode();
+        }
+        #endregion
+    }
+}
